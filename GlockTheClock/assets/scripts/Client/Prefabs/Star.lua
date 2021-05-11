9cddae5210181a718d07ac29d763795315a2a5a4
Adrian-de-Lucas-Gomez <47756657+Adrian-de-Lucas-Gomez@users.noreply.github.com>
2021-05-11 17:32:38 +0200

    Escena en blender
    Me da error al tratar de ejecutar el script

---
+++ GlockTheClock/assets/scripts/Client/Prefabs/Star.lua
@@ -0,0 +1,13 @@
+local pf = reqPrefab
+
+--Params holds required parameters to build an instance of this prefab. This will be populated with transform information from Blender via our script in /tools
+function pf.Star (params)
+	return{
+		Components = {
+			--Example user defined components
+		},
+		Transform = params.Transform,
+		Mesh = {mesh="Cube.mesh", material="Yellow"},
+        Rigidbody = {size=params.Transform.scale,mass=1,position=params.Transform.position,type=1,trigger=false,rotation=params.Transform.rotation}
+	}
+end
