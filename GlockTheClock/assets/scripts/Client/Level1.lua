0cf562c86f2b6261bf37203296ea2cca77873bab
Adrian-de-Lucas-Gomez <47756657+Adrian-de-Lucas-Gomez@users.noreply.github.com>
2021-05-13 10:34:05 +0200

    Ya se veeee y se puede jugar
    Lo he probado cin el pull del core donde se areglaba lo del raycast y va muy bien. Que felicidad :)
    
    El target es para que el mapa lo pille como prefab.

--- GlockTheClock/assets/scripts/Client/Level1.lua
+++ GlockTheClock/assets/scripts/Client/Level1.lua
@@ -1,13 +1,8 @@
 local prefabs = require('Prefab')
 local sceneConfig = require('Prefab')
 local Scene={
- Entities={	DeadZone_000=
-        {
-		Transform = {position={x=-34.03219223022461,y=-82.47428894042969,z=53.89710235595703},rotation={x=-0.0,y=0.0,z=0.0},scale={x=239.62815856933594,y=1.0,z=239.62815856933594}},
-            Components= {
-            }
-        }
-        ,
+ Entities={	DeadZone_000=prefabs.DeadZone({
+		Transform = {position={x=-34.03219223022461,y=-82.47428894042969,z=53.89710235595703},rotation={x=-0.0,y=0.0,z=0.0},scale={x=239.62815856933594,y=1.0,z=239.62815856933594}}}),
 	Goal=
         {
 		Transform = {position={x=-75.55284881591797,y=18.937252044677734,z=130.92279052734375},rotation={x=-0.0,y=0.0,z=0.0},scale={x=1.0,y=1.0,z=1.0}},
@@ -29,6 +24,8 @@ local Scene={
 		Transform = {position={x=-74.93299865722656,y=-4.676860809326172,z=52.011505126953125},rotation={x=-0.0,y=0.0,z=0.0},scale={x=13.536832809448242,y=17.694080352783203,z=12.35163688659668}}}),
 	Platform_006=prefabs.Platform({
 		Transform = {position={x=-74.93299865722656,y=5.99833869934082,z=115.08952331542969},rotation={x=-0.0,y=0.0,z=0.0},scale={x=13.536832809448242,y=7.076155662536621,z=27.246719360351562}}}),
+	Player_000=prefabs.Player({
+		Transform = {position={x=9.099167823791504,y=29.44719123840332,z=-21.36337661743164},rotation={x=-0.0,y=0.0,z=0.0},scale={x=1.0,y=1.0,z=1.0}}}),
 	Spawn=
         {
 		Transform = {position={x=9.335329055786133,y=18.937252044677734,z=-21.237735748291016},rotation={x=-0.0,y=0.0,z=0.0},scale={x=1.0,y=1.0,z=1.0}},
@@ -38,73 +35,28 @@ local Scene={
         ,
 	Star_000=prefabs.Star({
 		Transform = {position={x=-25.016244888305664,y=6.011415004730225,z=35.112457275390625},rotation={x=-0.0,y=0.0,z=90.00000250447816},scale={x=1.530989408493042,y=1.530989408493042,z=1.530989408493042}}}),
-	Target_000=
-        {
-		Transform = {position={x=9.099167823791504,y=18.09440040588379,z=-2.6706790924072266},rotation={x=-0.0,y=0.0,z=0.0},scale={x=1.0,y=1.0,z=1.0}},
-            Components= {
-            }
-        }
-        ,
-	Target_001=
-        {
-		Transform = {position={x=14.165214538574219,y=18.09440040588379,z=18.393634796142578},rotation={x=-0.0,y=0.0,z=0.0},scale={x=1.0,y=1.0,z=1.0}},
-            Components= {
-            }
-        }
-        ,
-	Target_002=
-        {
-		Transform = {position={x=4.155189514160156,y=18.09440040588379,z=18.041988372802734},rotation={x=-0.0,y=0.0,z=0.0},scale={x=1.0,y=1.0,z=1.0}},
-            Components= {
-            }
-        }
-        ,
-	Target_003=
-        {
-		Transform = {position={x=-24.918935775756836,y=23.818458557128906,z=26.3292236328125},rotation={x=-0.0,y=0.0,z=0.0},scale={x=1.0,y=1.0,z=1.0}},
-            Components= {
-            }
-        }
-        ,
-	Target_004=
-        {
-		Transform = {position={x=-34.626441955566406,y=29.45375633239746,z=26.3292236328125},rotation={x=-0.0,y=0.0,z=0.0},scale={x=1.0,y=1.0,z=1.0}},
-            Components= {
-            }
-        }
-        ,
-	Target_005=
-        {
-		Transform = {position={x=-43.466041564941406,y=23.465904235839844,z=26.3292236328125},rotation={x=-0.0,y=0.0,z=-90.00000250447816},scale={x=1.0,y=1.0,z=1.0}},
-            Components= {
-            }
-        }
-        ,
-	Target_006=
-        {
-		Transform = {position={x=-75.82684326171875,y=23.737550735473633,z=65.14886474609375},rotation={x=-0.0,y=0.0,z=0.0},scale={x=1.0,y=1.0,z=1.0}},
-            Components= {
-            }
-        }
-        ,
-	Target_007=
-        {
-		Transform = {position={x=-75.82684326171875,y=29.54666519165039,z=76.53772735595703},rotation={x=-0.0,y=0.0,z=0.0},scale={x=1.0,y=1.0,z=1.0}},
-            Components= {
-            }
-        }
-        ,
-	Target_008=
-        {
-		Transform = {position={x=-75.82684326171875,y=20.084461212158203,z=107.09172821044922},rotation={x=-0.0,y=0.0,z=0.0},scale={x=1.0,y=1.0,z=1.0}},
-            Components= {
-            }
-        }
-        },
+	Target_000=prefabs.Target({
+		Transform = {position={x=9.099167823791504,y=18.09440040588379,z=-2.6706790924072266},rotation={x=-0.0,y=0.0,z=0.0},scale={x=1.0,y=1.0,z=1.0}}}),
+	Target_001=prefabs.Target({
+		Transform = {position={x=14.165214538574219,y=18.09440040588379,z=18.393634796142578},rotation={x=-0.0,y=0.0,z=0.0},scale={x=1.0,y=1.0,z=1.0}}}),
+	Target_002=prefabs.Target({
+		Transform = {position={x=4.155189514160156,y=18.09440040588379,z=18.041988372802734},rotation={x=-0.0,y=0.0,z=0.0},scale={x=1.0,y=1.0,z=1.0}}}),
+	Target_003=prefabs.Target({
+		Transform = {position={x=-24.918935775756836,y=23.818458557128906,z=26.3292236328125},rotation={x=-0.0,y=0.0,z=0.0},scale={x=1.0,y=1.0,z=1.0}}}),
+	Target_004=prefabs.Target({
+		Transform = {position={x=-34.626441955566406,y=29.45375633239746,z=26.3292236328125},rotation={x=-0.0,y=0.0,z=0.0},scale={x=1.0,y=1.0,z=1.0}}}),
+	Target_005=prefabs.Target({
+		Transform = {position={x=-43.466041564941406,y=23.465904235839844,z=26.3292236328125},rotation={x=-0.0,y=0.0,z=-90.00000250447816},scale={x=1.0,y=1.0,z=1.0}}}),
+	Target_006=prefabs.Target({
+		Transform = {position={x=-75.82684326171875,y=23.737550735473633,z=65.14886474609375},rotation={x=-0.0,y=0.0,z=0.0},scale={x=1.0,y=1.0,z=1.0}}}),
+	Target_007=prefabs.Target({
+		Transform = {position={x=-75.82684326171875,y=29.54666519165039,z=76.53772735595703},rotation={x=-0.0,y=0.0,z=0.0},scale={x=1.0,y=1.0,z=1.0}}}),
+	Target_008=prefabs.Target({
+		Transform = {position={x=-75.82684326171875,y=20.084461212158203,z=107.09172821044922},rotation={x=-0.0,y=0.0,z=0.0},scale={x=1.0,y=1.0,z=1.0}}})},
 		SceneConfig=
         {
         PhysicsConfig={
-            Gravity = 5
+            Gravity = -100
         }
     }
 }
