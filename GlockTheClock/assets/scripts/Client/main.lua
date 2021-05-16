5b0c7f3ec7a3e183d1f6971dd1c87ccfb784aea8
AdriAlvarez00 <adralv01@ucm.es>
2021-05-16 20:11:39 +0200

    main.lua para el pull #70

---
+++ GlockTheClock/assets/scripts/Client/main.lua
@@ -0,0 +1,15 @@
+local ns = require('namespace')
+
+
+
+local status, ret = pcall(ns.loadScene, Manager, require('sampleScene'))
+if status then
+	LOG("Scene loaded correctly")
+else
+	local message = tostring("Fatal error loading scene: " .. ret)
+	LOG(message, LogLevel.Error, 1)
+end
+-- Showing component Added event working
+--local ents = Manager:getEntitiesWithComponent("playerMove")
+--if ents ~= {} then ents[1]:add(ns.Component.all["topo"]()) end
+LOG("main.lua completed")
