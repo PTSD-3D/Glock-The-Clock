1b60af1ae94ccfb05179b1f0fe5bc055d485ae0b
Esteban <35503563+EstebenR@users.noreply.github.com>
2021-05-18 11:23:15 +0200

    Listas de inicializacion (#10)
    * Creadas listas
    
    * Añadidos prefabs y sistemas faltantes

---
+++ GlockTheClock/assets/scripts/Client/PrefabsList.lua
@@ -0,0 +1,12 @@
+--New Prefabs are to be required here for loading
+
+require('Bullet')
+require('Enemy')
+require('BlueTarget')
+require('DeadZone')
+require('Kerbo')
+require('Platform')
+require('Player')
+require('RedTarget')
+
+require('sampleScene-config')
