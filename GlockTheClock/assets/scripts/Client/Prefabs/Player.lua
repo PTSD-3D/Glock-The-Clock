7c57cc7e0b806337fa4cc43b3b6190baf80b4356
Will7461 <43720307+Will7461@users.noreply.github.com>
2021-05-19 18:18:12 +0200

    Efectos de sonido (#12)
    * sonidos
    
    * Sonido para botones
    
    * upload musica
    
    * Implementados algunos sonidos

--- GlockTheClock/assets/scripts/Client/Prefabs/Player.lua
+++ GlockTheClock/assets/scripts/Client/Prefabs/Player.lua
@@ -1,11 +1,14 @@
 local pf = require('Prefab')
 
+local resources = require('resources')
+
 -- Params holds required parameters to build an instance of this prefab. This will be populated with transform information from Blender via our script in /tools
 function pf.Player(params)
 	return {
 		Components = {
 			-- Example user defined components
-			{ name = "playerMove", arguments = { 100, 3, 1500 } }
+			{ name = "playerMove", arguments = { 100, 3, 1500 } },
+			{ name = "boombox", arguments={0.5, false, resources.Sounds.Level1Theme, -1}}
 		},
 		Transform = params.Transform,
         Rigidbody = {size={x=1,y=1,z=1},mass=1,position={x=0,y=0,z=0},type=0,trigger=false,rotation={x=0,y=0,z=0,w=1}},
