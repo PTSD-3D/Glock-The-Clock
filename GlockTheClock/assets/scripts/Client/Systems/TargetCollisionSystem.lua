5a078bfa0223a13ab6eb67d06c6a72f803876d7b
Adrian-de-Lucas-Gomez <adridelu@ucm.es>
2021-05-25 16:28:28 +0200

    Puedes romper dianas con el cuerpo
    Era la gracia que tenia el primer nivel y es algo que no hace daño a nadie

--- GlockTheClock/assets/scripts/Client/Systems/TargetCollisionSystem.lua
+++ GlockTheClock/assets/scripts/Client/Systems/TargetCollisionSystem.lua
@@ -7,7 +7,7 @@ local TargetCollisionSystem = ns.class("TargetCollisionSystem", ns.System)
 function TargetCollisionSystem:requires() return {"targetCollision"} end
 
 function TargetCollisionSystem:onCollision(target, other, collision)
-	if(other:has("bullet")) then
+	if(other:has("bullet") or other:has("playerMove")) then	--The player can break the targets with his body too
 		local targetCollision = target:get("targetCollision")
 		Manager:removeEntity(target)
 
