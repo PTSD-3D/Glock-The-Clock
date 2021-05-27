a6f7549ae0edfaa1a869641f672173fb515460a9
David Godoy Ruiz <dagodoy@ucm.es>
2021-05-27 19:15:23 +0200

    Patina menos

--- GlockTheClock/assets/scripts/Client/Prefabs/Player.lua
+++ GlockTheClock/assets/scripts/Client/Prefabs/Player.lua
@@ -7,7 +7,7 @@ function pf.Player(params)
 	return {
 		Components = {
 			-- Example user defined components
-			{ name = "playerMove", arguments = { 100, 0.1, 2000, 4 } },
+			{ name = "playerMove", arguments = { 100, 0.1, 2000, 12 } },
 			{ name = "boombox", arguments={0.5, false, resources.Sounds.Level1Theme, -1}}
 		},
 		Transform = params.Transform,
